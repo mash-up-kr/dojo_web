e69d9821b22f7055e76232193fbcb332ab356e59
2-NOW <kj109888@gmail.com>
2024-06-16 16:35:59 +0900

    update tanstack-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,28 @@
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { routeTree } from "./routeTree.gen";
 import "./index.css";
 import "./tailwindcss.css";
+
+// Register things for typesafety
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
+}
+
+// Set up a Router instance
+const router = createRouter({
+	routeTree,
+	defaultPreload: "intent",
+});
 
 const root = document.getElementById("root");
 if (root) {
 	ReactDOM.createRoot(root).render(
 		<React.StrictMode>
-			<App />
+			<RouterProvider router={router} />
 		</React.StrictMode>,
 	);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import path from "node:path";
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), svgr()],
+	plugins: [react(), svgr(), TanStackRouterVite()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
