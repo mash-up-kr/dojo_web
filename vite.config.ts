3c6d5d963c026f3d1184d07949a9d083335d5ebc
2-NOW <kj109888@gmail.com>
2024-06-16 16:07:42 +0900

    migration nextjs to react only

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import path from "node:path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [react(), svgr()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+});
