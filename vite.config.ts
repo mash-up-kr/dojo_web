9c21d3f19f8546fe6f05867e8c8043a05f893d8b
2-NOW <kj109888@gmail.com>
2024-07-27 17:07:15 +0900

    feat: change tanstack route to react-router-dom

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import path from "node:path";
-import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), TanStackRouterVite()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
