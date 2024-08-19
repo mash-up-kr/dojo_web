3194ddc6b6b5b18b47367449a6712dac5165320f
happyGyu <happyboyto.dev@gmail.com>
2024-08-19 20:08:37 +0900

    feat: 헤더 프로필 버튼 수정 및 링크 수정

--- src/pages/vote/VoteLayout.tsx
+++ src/pages/vote/VoteLayout.tsx
@@ -1,5 +1,7 @@
+import NavIcon from "@/assets/ic_12_menu.svg?react";
 import { Header, HeaderLogo } from "@/components/common/Header";
 import Image from "@/components/common/Image";
+import { useMe } from "@/generated/member/member";
 import { Link } from "@/stackflow/Link";
 import { getGradientBg } from "@/utils/getGradientBg";
 import clsx from "clsx";
@@ -7,14 +9,23 @@ import clsx from "clsx";
 const bg = getGradientBg();
 
 export function VoteLayout({ children }: { children: React.ReactNode }) {
+  const { data: meRes } = useMe();
+
   return (
     <div className={clsx("min-h-screen", bg)}>
       <Header
         left={<HeaderLogo />}
-        title="테스트입니다"
         right={
-          <Link activityName="MyPickPage" activityParams={{}}>
-            <Image className="w-8 h-8 bg-gray084 rounded-full" />
+          <Link
+            activityName="SpacePage"
+            activityParams={{ memberId: meRes?.data?.memberId ?? "" }}
+            className="relative"
+          >
+            <Image
+              src={meRes?.data?.profileImageUrl}
+              className="w-8 h-8 bg-gray084 rounded-full border border-gray064"
+            />
+            <NavIcon className="absolute -bottom-[1px] -right-[1px]" />
           </Link>
         }
       />
