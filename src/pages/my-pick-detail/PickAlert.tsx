fe7f2993a74b05b0e1e3c3509aa5c6942076b235
minsgy <alstjrchl97@gmail.com>
2024-08-24 01:49:15 +0900

    fix: alert image url

--- src/pages/my-pick-detail/PickAlert.tsx
+++ src/pages/my-pick-detail/PickAlert.tsx
@@ -54,7 +54,11 @@ export const PickAlert = ({
         </p>
         <div className="space-y-3 text-center mt-6">
           {pickOpen.pickOpenImageUrl && (
-            <Image src={pickOpen.pickOpenImageUrl} alt="프로필" />
+            <Image
+              src={pickOpen.pickOpenImageUrl}
+              alt="프로필"
+              className="w-[84px] h-[84px]"
+            />
           )}
           <span className="t-h3-b-22 inline-block">
             {getPickOpenValueText(
