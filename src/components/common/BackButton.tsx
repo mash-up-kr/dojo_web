033b48fe0c6516eb659abf13d17de22185d00c8e
happyGyu <happyboyto.dev@gmail.com>
2024-08-23 23:16:52 +0900

    fix: stack acticity 없을 때 기본으로 vote page로 떨굼

--- src/components/common/BackButton.tsx
+++ src/components/common/BackButton.tsx
@@ -1,11 +1,21 @@
 import Back from "@/assets/ic_24_back.svg?react";
 import { useMyFlow } from "@/stackflow/useMyFlow";
+import { useStack } from "@stackflow/react";
 
 export const BackButton = () => {
-  const { pop } = useMyFlow();
+  const stack = useStack();
+  const { pop, push } = useMyFlow();
+
+  const onClick = () => {
+    if (stack.activities.length <= 1) {
+      push("VotePage", {}, { animate: false });
+    } else {
+      pop();
+    }
+  };
 
   return (
-    <button type="button" onClick={pop}>
+    <button type="button" onClick={onClick}>
       <Back />
     </button>
   );
