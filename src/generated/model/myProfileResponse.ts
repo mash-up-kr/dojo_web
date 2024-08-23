bc93395cdfc7d455e3cd0407bee87b125569dac3
2-NOW <kj109888@gmail.com>
2024-08-23 22:16:41 +0900

    gen api

--- src/generated/model/myProfileResponse.ts
+++ src/generated/model/myProfileResponse.ts
@@ -9,8 +9,6 @@
  * 프로필 응답 Response
  */
 export interface MyProfileResponse {
-  /** 소유한 코인 개수 */
-  coinCount: number;
   /** 유저의 친구 수 */
   friendCount: number;
   /** 유저 id */
@@ -21,6 +19,8 @@ export interface MyProfileResponse {
   ordinal: number;
   /** 유저가 받은 픽 개수 */
   pickCount: number;
+  /** 유저가 받은 픽 개수 */
+  pickedCount: number;
   /** 유저 플랫폼 */
   platform: string;
   /** 유저 프로필 이미지 url */
