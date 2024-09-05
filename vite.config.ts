a75669fc6b7e874090d775bad2b274bb74f1f696
minsgy <alstjrchl97@gmail.com>
2024-09-05 23:19:11 +0900

    feat: favicon

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import path from "node:path";
+import favicons from "@peterek/vite-plugin-favicons";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), favicons("src/assets/favicon.svg")],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
