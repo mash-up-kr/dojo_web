37b868d62ecd0d17c8f983dfc70e332ca93d5c26
happyGyu <happyboyto.dev@gmail.com>
2024-09-07 13:54:17 +0900

    test: 알림 끄기

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,15 @@
-import { EventSourcePolyfill } from "event-source-polyfill";
-import Cookies from "js-cookie";
-import { useEffect } from "react";
+// import { EventSourcePolyfill } from "event-source-polyfill";
+// import Cookies from "js-cookie";
+// import { useEffect } from "react";
 import { Toaster } from "sonner";
-import { Toast } from "./components/common/Toast";
-import { useMe } from "./generated/member/member";
-import { useMyFlow } from "./stackflow/useMyFlow";
+// import { Toast } from "./components/common/Toast";
+// import { useMe } from "./generated/member/member";
+// import { useMyFlow } from "./stackflow/useMyFlow";
 
 export function App({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <SSEAlert />
+      {/* <SSEAlert /> */}
       {children}
       <Toaster
         position="top-center"
@@ -22,44 +22,44 @@ export function App({ children }: { children: React.ReactNode }) {
   );
 }
 
-function SSEAlert() {
-  const { push } = useMyFlow();
-  const { data } = useMe();
+// function SSEAlert() {
+//   const { push } = useMyFlow();
+//   const { data } = useMe();
 
-  useEffect(() => {
-    const token = Cookies.get("token");
-    if (!token) {
-      return;
-    }
+//   useEffect(() => {
+//     const token = Cookies.get("token");
+//     if (!token) {
+//       return;
+//     }
 
-    const src = new EventSourcePolyfill(
-      "https://docker-ecs.net/notification-stream",
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-        heartbeatTimeout: 180000,
-      },
-    );
+//     const src = new EventSourcePolyfill(
+//       "https://docker-ecs.net/notification-stream",
+//       {
+//         headers: {
+//           Authorization: `Bearer ${token}`,
+//         },
+//         heartbeatTimeout: 180000,
+//       },
+//     );
 
-    src.addEventListener("picked", (v) => {
-      const pick = JSON.parse((v as MessageEvent).data);
-      const myname = data?.data?.memberName
-        ? `${data?.data?.memberName}님을`
-        : "나를";
-      Toast.alert(`방금 누군가가 ${myname} Pick 했어요!`, () => {
-        push("MyPickDetailPage", { questionId: pick?.questionId ?? "" });
-      });
-    });
+//     src.addEventListener("picked", (v) => {
+//       const pick = JSON.parse((v as MessageEvent).data);
+//       const myname = data?.data?.memberName
+//         ? `${data?.data?.memberName}님을`
+//         : "나를";
+//       Toast.alert(`방금 누군가가 ${myname} Pick 했어요!`, () => {
+//         push("MyPickDetailPage", { questionId: pick?.questionId ?? "" });
+//       });
+//     });
 
-    src.onopen = () => {
-      console.log("open");
-    };
+//     src.onopen = () => {
+//       console.log("open");
+//     };
 
-    return () => {
-      src.close();
-    };
-  }, [data?.data?.memberName, push]);
+//     return () => {
+//       src.close();
+//     };
+//   }, [data?.data?.memberName, push]);
 
-  return <></>;
-}
+//   return <></>;
+// }
